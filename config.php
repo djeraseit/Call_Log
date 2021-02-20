65255b4000be8b9596a1d307cb00309191e75ea7
djeraseit <theodis@theodis.com>
2021-02-19 22:34:21 -0600

    update

--- config.php
+++ config.php
@@ -3,6 +3,15 @@
 error_reporting(E_ALL); 
 ini_set( 'display_errors','1');
 
+ // is SimpleXML installed
+ if (!class_exists('SimpleXMLElement')){
+    die('Sorry php-xml is not installed or php is not compiled with libxml!');
+}
+ // is cURL installed 
+ if (!function_exists('curl_init')){
+    die('Sorry cURL is not installed!');
+}
+
 $config = [];
 $config['numverifykey'] = 'xxx';
 $config['twiliosid'] = getenv("TWILIO_ACCOUNT_SID"); // use nvram
@@ -10,3 +19,4 @@ $config['twiliotoken'] = getenv("TWILIO_AUTH_TOKEN"); // use nvram
 $config['obiusername'] = "admin";
 $config['obipassword'] = "megalith";
 $config['obihost'] = "192.168.42.2";
+$config['pushbullettoken'] = 'o.mjCLA2hY2n5jVnwGwHrIDO76KccJtIbl';
--- lastcaller.php
+++ lastcaller.php
@@ -1,5 +1,6 @@
 <?php
 require_once(__DIR__.'/config.php');
+require_once(__DIR__.'/functions.php');
 error_reporting(E_ALL); 
 ini_set( 'display_errors','1');
 $opencnam = 'https://api.opencnam.com/v2/phone/+15555555555';
