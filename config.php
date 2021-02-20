9333b8a3ec3fd8f4c87af7f3d0209b640475db1e
djeraseit <theodis@theodis.com>
2021-02-19 19:45:33 -0600

    add array

--- config.php
+++ config.php
@@ -1,5 +1,8 @@
 <?php
 
-$numverifykey = 'xxx';
-$twiliosid = getenv("TWILIO_ACCOUNT_SID");
-$twiliotoken = getenv("TWILIO_AUTH_TOKEN");
+$config = [];
+$config['numverifykey'] = 'xxx';
+$config['twiliosid'] = getenv("TWILIO_ACCOUNT_SID"); // use nvram
+$config['twiliotoken'] = getenv("TWILIO_AUTH_TOKEN"); // use nvram
+$config['obiusername'] = "admin";
+$config['obipassword'] = "megalith";
