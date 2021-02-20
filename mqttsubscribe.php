4d51a120e77c3174fda6f604bf0a1ccee149b461
djeraseit <theodis@theodis.com>
2021-02-20 01:25:40 -0600

    statelines

--- mqttsubscribe.php
+++ mqttsubscribe.php
@@ -8,11 +8,13 @@ if (isset($config['cloudamqphost'])) {
     $username = $config['cloudamqpuser'];
     $password = $config['cloudamqppass'];
     $port = $config['cloudamqpport'];
+    $topic = $config['cloudamqptopic'];
   } else {
   $host = '192.168.42.2';
   $username = "admin";
   $password = "admin";
   $port = 1883;
+  $topic = "obihai" . "/state_line" . "1"; // either one or 2
   }
 
 $mqtt = new bluerhinos\phpMQTT($host, $port, "ClientID".rand());
@@ -22,7 +24,7 @@ if(!$mqtt->connect(true,NULL,$username,$password)){
 }
 
 //currently subscribed topics
-$topics['obihai'] = array("qos"=>0, "function"=>"procmsg");
+$topics[$topic] = array("qos"=>0, "function"=>"procmsg");
 $mqtt->subscribe($topics,0);
 
 while($mqtt->proc()){
