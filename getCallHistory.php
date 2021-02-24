8f132c099abef476155b46aef6ab2a5697e85ab9
djeraseit <theodis@theodis.com>
2021-02-24 00:58:26 -0600

    Get call history

---
+++ getCallHistory.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+* Megalith Technologies
+*
+* @category VoIP
+* @author Theodis Butler
+* @copyright Copyright (c) 2021 Megalith Technologies (https://megalithtechnologies.com)
+* @license https://www.gnu.org/licenses/gpl-3.0.en.html
+*
+* Call Blocker for stopping annoying, spam and robocalls on Obihai Devices
+* Copyright (C) 2021 Theodis Butler
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see https://www.gnu.org/licenses/gpl-3.0.en.html.
+*/
+
+// This is for datatables web interface AJAX
+
+$config = require __DIR__.'/config.php';
+require_once(__DIR__.'/functions.php');
+
+
+if (isset($config['obihai']['host'])) {
+   $host = $config['obihai']['host'];
+   $username = $config['obihai']['credentials']['username'];
+   $password = $config['obihai']['credentials']['password'];
+   $scheme = $config['obihai']['scheme'];
+ } else {
+ $host = '192.168.42.2';
+ $username = "admin";
+ $password = "admin";
+ $scheme = "http";
+}
+
+$pagename = 'callhistory.xml';
+
+$url = "{$scheme}://{$host}/{$pagename}";
+
+
+$options = array(
+        CURLOPT_URL            => $url,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_IGNORE_CONTENT_LENGTH=> true,
+        CURLOPT_TCP_KEEPALIVE => 1,
+        CURLOPT_TCP_KEEPIDLE => 2,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_SSL_VERIFYPEER => false,    // for https
+        CURLOPT_USERPWD        => $username . ":" . $password,
+        CURLOPT_HTTPAUTH       => CURLAUTH_DIGEST,
+        CURLOPT_POST           => true,
+        CURLOPT_FAILONERROR => true,
+        CURLOPT_TIMEOUT => 5,
+        CURLOPT_CONNECTTIMEOUT => 5,
+        CURLOPT_NOPROXY => '*', // do not use proxy
+);
+
+$ch = curl_init();
+
+curl_setopt_array( $ch, $options );
+
+try {
+  $raw_response  = curl_exec( $ch );
+
+  // validate CURL status
+  if(curl_errno($ch))
+     throw new Exception(curl_error($ch), 500);
+  // validate HTTP status code (user/password credential issues)
+  $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+  if ($status_code != 200)
+      throw new Exception("Response with Status Code [" . $status_code . "].", 500);
+}
+
+ catch(Exception $ex) {
+    if ($ch != null) curl_close($ch);
+     throw new Exception($ex);
+}
+
+$jsonresponse = json_decode(callHistoryJson($raw_response),true);
+$dataTable = array('data'=>$jsonresponse);
+echo json_encode($dataTable);
